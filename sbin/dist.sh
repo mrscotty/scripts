ba9831620712437e909af94e007d4fb9bec09490
Scott T. Hardin <scott.hardin@hnsc.de>
2010-03-18 14:18:48 +0100

    added full path to xml files

--- sbin/dist.sh
+++ sbin/dist.sh
@@ -2,7 +2,7 @@
 #
 # Copy files that must land in the distro directories
 
-
+local=/etc/openxpki/local
 xmldir=instances/trustcenter1
 for i in $xmldir/config.xml $xmldir/acl.xml $xmldir/workflow.xml $xmldir/workflow_condition.xml; do
 	dest=`dirname $i`
@@ -11,5 +11,5 @@ for i in $xmldir/config.xml $xmldir/acl.xml $xmldir/workflow.xml $xmldir/workflo
 		cp -p /etc/openxpki/$i /etc/openxpki/$i.orig
 	fi
 	echo "Copying $i"
-	cp -p $i /etc/openxpki/$dest/
+	cp -p $local/$i /etc/openxpki/$dest/
 done
