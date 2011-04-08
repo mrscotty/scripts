852cf13e40feafe8d09c4409a659df37b3bc0280
Scott T. Hardin <scott.hardin@hnsc.de>
2011-04-08 11:03:38 +0200

    rewrote keygroup.pl to use new test module

--- sbin/keygroup.pl
+++ sbin/keygroup.pl
@@ -8,123 +8,11 @@
 #   openssl genrsa -out /tmp/sc_pers_csr.key 1024
 
 use strict;
+use warnings;
 
-use OpenXPKI::Tests;
-use OpenXPKI::Client;
-use Data::Dumper;
+use OpenXPKI::Tests::More;
 use Carp;
-
-# use test deployment
-my $instancedir = '';
-my $socketfile  = $instancedir . '/var/openxpki/openxpki.socket';
-my $pidfile     = $instancedir . '/var/openxpki/openxpki.pid';
-
-my $wf_type   = 'I18N_OPENXPKI_WF_TYPE_SMARTCARD_PERSONALIZATION';
-my $USER      = 'ca';
-my $USERROLE = 'CA Operator';
-
-my ( $msg, $wf_id, $wf_info, $client, $pkcs10, $csr );
-
-#
-# $client = wfconnect( USER, PASS );
-#
-sub wfconnect {
-    my ( $u, $p ) = @_;
-    my $c = OpenXPKI::Client->new(
-        {   TIMEOUT    => 100,
-            SOCKETFILE => $instancedir . '/var/openxpki/openxpki.socket',
-        }
-    );
-    login(
-        {   CLIENT   => $c,
-            USER     => $u,
-            PASSWORD => $p,
-        }
-    ) or die "Login as $c failed: $@";
-    return $c;
-}
-
-sub wfdisconnect {
-    my $c = $_[0] || $client;
-    eval { $c && $c->send_receive_service_msg('LOGOUT'); };
-    unless ( $_[0] ) {
-        $client = undef;
-    }
-}
-
-#
-# usage: my $msg = wfexec( ID, ACTIVITY, { PARAMS } );
-#
-sub wfexec {
-    return wfexec2( $client, @_ );
-
-    my ( $id, $act, $params ) = @_;
-    my $msg;
-    croak("Unable to exec action '$act' on closed connection")
-        unless defined $client;
-
-    $msg = $client->send_receive_command_msg(
-        'execute_workflow_activity',
-        {   'ID'       => $id,
-            'ACTIVITY' => $act,
-            'PARAMS'   => $params,
-            'WORKFLOW' => $wf_type,
-        },
-    );
-    return $msg;
-
-}
-
-#
-# usage: my $msg = wfexec( CLIENT, ID, ACTIVITY, { PARAMS } );
-#
-sub wfexec2 {
-    my ( $client, $id, $act, $params ) = @_;
-    my $msg;
-
-    croak("Unable to exec action '$act' on closed connection")
-        unless defined $client;
-
-    $msg = $client->send_receive_command_msg(
-        'execute_workflow_activity',
-        {   'ID'       => $id,
-            'ACTIVITY' => $act,
-            'PARAMS'   => $params,
-            'WORKFLOW' => $wf_type,
-        },
-    );
-    return $msg;
-}
-
-#
-# usage: my $state = wfstate( USER, PASS, ID );
-# Note: $@ contains either error message or Dumper($msg)
-#
-sub wfstate {
-    my ( $user, $pass, $id ) = @_;
-    my ( $msg, $state );
-    my $disc = 0;
-    $@ = '';
-
-    unless ($client) {
-        $disc++;
-        unless ( $client = wfconnect( $user, $pass ) ) {
-            $@ = "Failed to connect as $user";
-            return;
-        }
-    }
-    $msg = $client->send_receive_command_msg( 'get_workflow_info',
-        { 'WORKFLOW' => $wf_type, 'ID' => $id, } );
-    if ( is_error_response($msg) ) {
-        $@ = "Error running get_workflow_info: '" . Dumper($msg) . "'";
-        return;
-    }
-    $@ = Dumper($msg);
-    if ($disc) {
-        wfdisconnect();
-    }
-    return $msg->{PARAMS}->{WORKFLOW}->{STATE};
-}
+use Data::Dumper;
 
 sub maySucceed {
     my $msg = shift;
@@ -149,10 +37,28 @@ sub mustSucceed {
     }
 }
 
-$client = wfconnect($USER, $USERROLE);
-$msg = $client->send_receive_command_msg(
-    'set_secret_part',
-    { 'SECRET' => 'default', 'VALUE' => '1234567890', },
+# use test deployment
+my $instancedir = '';
+my $socketfile  = $instancedir . '/var/openxpki/openxpki.socket';
+my $pidfile     = $instancedir . '/var/openxpki/openxpki.pid';
+
+my $wf_type  = 'I18N_OPENXPKI_WF_TYPE_SMARTCARD_PERSONALIZATION';
+my $USER     = 'ca';
+my $USERROLE = 'CA Operator';
+
+my $test = OpenXPKI::Tests::More->new(
+    {
+        socketfile => $socketfile,
+        realm      => 'User TEST CA'
+    },
 );
+
+$test->connect( user => $USER, password => $USERROLE );
+my $client = $test->get_client();
+
+my $msg =
+  $client->send_receive_command_msg( 'set_secret_part',
+    { 'SECRET' => 'default', 'VALUE' => '1234567890', },
+  );
 mustSucceed($msg);
 
