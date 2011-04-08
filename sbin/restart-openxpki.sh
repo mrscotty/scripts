b46cafe30dc1518d5400f0f3320ea763c1a7800e
Scott T. Hardin <scott@hnsc.de>
2011-04-08 10:48:49 +0200

    make restart-openxpki.sh sudo-friendly
    - added sbin/truncfile
    - added sudo to restart script

--- sbin/restart-openxpki.sh
+++ sbin/restart-openxpki.sh
@@ -1,18 +1,33 @@
 #!/bin/sh
+#
+# restart-openxpki.sh
+#
+# This shell script is for use by the developer during testing. It can
+# run as non-root and will restart the openxpki server after truncating
+# the log files.
+#
+# It looks in the same directory for the helper scripts.
 
-openxpkictl stop
->/var/openxpki/stderr.log
->/var/openxpki/openxpki.log
+dir=`dirname $0`
+
+sudo openxpki openxpkictl stop
+
+sudo openxpki ${dir}/truncfile \
+    /var/openxpki/stderr.log \
+    /var/openxpki/openxpki.log
+
 #  --debug '.*WorkflowCondition.*:128' \
 #  --debug '.*Notification.*:128' \
 #  --debug '.*Dispatcher.*:128' \
 #  --debug '.*Datapool.*:128' \
-openxpkictl \
+sudo openxpki openxpkictl \
   --debug '.*CardAdm.*:64' \
+  --debug '.*FetchPUK.*:64' \
+  --debug '.*Default.*:64' \
   start
 echo "###############"
 ps -ef | grep ^openxpki
 echo "Sleeping before enabling key group"
 sleep 2
-/etc/openxpki/local/sbin/keygroup.pl
+${dir}/keygroup.pl
 echo "done."
