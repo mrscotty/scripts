b1ea832a9048a806263e81abd36d5350ce9418ea
Scott T. Hardin <scott.hardin@hnsc.de>
2012-07-26 18:18:31 +0200

    add oracle to reset-db.sh

--- sbin/reset-db.sh
+++ sbin/reset-db.sh
@@ -2,6 +2,16 @@
 #
 # reset-db.sh - reset workflow data for testing
 
+if [ -f /applications/oracle/10.2.0.4/bin/sqlplus ]; then
+    . /etc/sysconfig/openxpki && sqlplus / <<EOF
+delete from l2openxpki.config;
+delete from l2openxpki.certificate_attributes;
+delete from l2openxpki.certificate where role = 'User';
+delete from l2openxpki.workflow_history;
+delete from l2openxpki.workflow_context;
+delete from l2openxpki.workflow;
+EOF
+else
 #select * from certificate_attributes where exists (select * from certificate where certificate_attributes.identifier = certificate.identifier and certificate.role = 'User');
 mysql -u openxpki -popenxpki openxpki <<EOF
 delete from config;
@@ -11,3 +21,4 @@ delete from workflow_history;
 delete from workflow_context;
 delete from workflow;
 EOF
+fi
